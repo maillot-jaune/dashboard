3751621c9864715131ab074d3439951433c3351e
maillot-jaune <stefan@inizio.se>
2014-04-17 15:38:45 +0200

    jQuery

--- R/get_tweet.R
+++ R/get_tweet.R
@@ -1,10 +1,10 @@
-get_tweet <- function(){
+get_tweet <- function(x){
   
   load('~/Desktop/twitter_auth.Rdata')
   
   registerTwitterOAuth(twitCred)
   
-  d <- twListToDF(homeTimeline(n = 1))
+  d <- twListToDF(homeTimeline(n = x))
   d <- d[1, c(1, 11)] # 1=txt 11=usr
   return(d)
 }
