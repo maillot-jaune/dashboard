c3c7c6614c71dfc9b019ad8a446ed446e13ec811
maillot-jaune <stefan@inizio.se>
2014-04-17 15:08:55 +0200

    Directory relocation

--- R/get_tweet.R
+++ R/get_tweet.R
@@ -1,12 +1,10 @@
-# Test R-file
-# test
 get_tweet <- function(){
-  library(twitteR)
-
+  
   load('~/Desktop/twitter_auth.Rdata')
+  
   registerTwitterOAuth(twitCred)
   
   d <- twListToDF(homeTimeline(n = 1))
-  d <- d[1, 1]
-  return(d)
+  d <- d[1, c(1, 11)] # 1=txt 11=usr
+  resturn(d)
 }
