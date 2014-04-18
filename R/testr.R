5bb139aca99c1935e63a98077c262e6a1c67c7e7
maillot-jaune <stefan@inizio.se>
2014-04-18 11:21:19 +0200

    Testr

--- R/testr.R
+++ R/testr.R
@@ -1,4 +1,23 @@
 testr <- function(x){
-  y <- x + pi
-  return(y)
+#  y <- x + pi
+#  return(y)
+  tryCatch({
+      
+      local({
+          load('/home/stefan/Desktop/twitter_auth.Rdata')
+        })
+      
+    }, warning = function(w) {
+      # warning-handler-code
+      #cat('Warning')
+    }, error = function(e) {
+      # error-handler-code
+      #cat('Error, somethin went wrong\n')
+    }, finally = {
+      # cleanup-code
+      #cat('Nothin to do, set d to arbitrary string \n')
+      d <- 'Clean up and tweet me!'
+    })
+  
+  return(d)
 }
