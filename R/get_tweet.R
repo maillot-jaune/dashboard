f725a14842b7b60f70a1e519d6b8d967b6f7096c
maillot-jaune <stefan@inizio.se>
2014-04-18 12:37:26 +0200

    Testr

--- R/get_tweet.R
+++ R/get_tweet.R
@@ -4,29 +4,16 @@ get_tweet <- function(x){
   # authentication. So the tweet returned by this function 
   # will be the latest one in @si_petersson timeline.
 
-  tc <- file(description = '/home/stefan/Desktop/twitter_auth.Rdata', 
-    open = 'rb', 
-    blocking = TRUE, 
-    encoding = getOption('encoding')
-  )
-  
-  load(tc)
-  
-  #local({
-  #  load('/home/stefan/Desktop/twitter_auth.Rdata')
-  #})
-  #load('/home/stefan/Desktop/twitter_auth.Rdata')
-  #
-  # Authentcate...
-  
-  tryCatch({
-      
-      registerTwitterOAuth(twitCred)
-      d <- twListToDF(homeTimeline(n = x))
-      d <- d[1, 1] # 1=txt 11=usr
-      d <- as.character(d)
-      
-    }, warning = function(w) {
+ tryCatch({
+     tc <- file(description = '/home/stefan/Desktop/twitter_auth.Rdata', 
+       open = 'rb', 
+       blocking = TRUE, 
+       encoding = getOption('encoding')
+     )
+     
+     load(tc)
+ 
+   }, warning = function(w) {
       # warning-handler-code
       #cat('Warning')
     }, error = function(e) {
@@ -38,6 +25,34 @@ get_tweet <- function(x){
       d <- 'Clean up and tweet me!'
     })
  
+ 
+  
+  #local({
+  #  load('/home/stefan/Desktop/twitter_auth.Rdata')
+  #})
+  #load('/home/stefan/Desktop/twitter_auth.Rdata')
+  #
+  # Authentcate...
+  
+#  tryCatch({
+#      
+#      registerTwitterOAuth(twitCred)
+#      d <- twListToDF(homeTimeline(n = x))
+#      d <- d[1, 1] # 1=txt 11=usr
+#      d <- as.character(d)
+#      
+#    }, warning = function(w) {
+#      # warning-handler-code
+#      #cat('Warning')
+#    }, error = function(e) {
+#      # error-handler-code
+#      #cat('Error, somethin went wrong\n')
+#    }, finally = {
+#      # cleanup-code
+#      #cat('Nothin to do, set d to arbitrary string \n')
+#      d <- 'Clean up and tweet me!'
+#    })
+# 
   #registerTwitterOAuth(twitCred)  
   #
   # Clean up
@@ -46,5 +61,6 @@ get_tweet <- function(x){
   #d <- as.character(d)
   #
   # Return a character string
+  d <- 1
   return(d)
 }
