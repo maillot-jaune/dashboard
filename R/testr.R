20ecc8ff326df26747bdf76aae02bf1a950c3ff6
maillot-jaune <stefan@inizio.se>
2014-04-18 12:15:13 +0200

    Testr

--- R/testr.R
+++ R/testr.R
@@ -3,11 +3,11 @@ testr <- function(x){
 #  return(y)
   d <- tryCatch({
 
-#          load('/home/stefan/Desktop/twitter_auth.Rdata')
-          load('../../../../../../../home/stefan/Desktop/twitter_auth.Rdata')
+         load('/home/stefan/Desktop/twitte_auth.Rdata')
+#         load('../../../../../../../home/stefan/Desktop/twitter_auth.Rdata')
 
     }, warning = function(w) {
-      d <- paste('Warning: <pre>', w, '\n\n\n', getwd(), '\n\n\n' ,sessionInfo())
+      d <- paste('Warning: ', w, getwd())
     }, error = function(e) {
       d <- paste('Error:', e)
     }, finally = {
