46ae1f7c67063706d04f19bb8193cd2a0fcf3994
maillot-jaune <stefan@inizio.se>
2014-04-18 11:13:36 +0200

    tryCatch for twitter auth

--- R/get_tweet.R
+++ R/get_tweet.R
@@ -4,18 +4,41 @@ get_tweet <- function(x){
   # authentication. So the tweet returned by this function 
   # will be the latest one in @si_petersson timeline.
   local({
-      load('/home/stefan/Desktop/twitter_auth.Rdata')
-    })
+    load('/home/stefan/Desktop/twitter_auth.Rdata')
+  })
   #load('/home/stefan/Desktop/twitter_auth.Rdata')
   #
   # Authentcate...
-  registerTwitterOAuth(twitCred)
+      
+  tryCatch({
+      
+      registerTwitterOAuth(twitCred)
+      d <- twListToDF(homeTimeline(n = x))
+      d <- d[1, 1] # 1=txt 11=usr
+      d <- as.character(d)
+      
+    }, warning = function(w) {
+      # warning-handler-code
+      cat('Warning')
+    }, error = function(e) {
+      # error-handler-code
+      #cat('Error, somethin went wrong\n')
+    }, finally = {
+      # cleanup-code
+      #cat('Nothin to do, set d to arbitrary string \n')
+      d <- 'Clean up and tweet me!'
+    })
+  
+
+  #registerTwitterOAuth(twitCred)  
   #
   # Clean up
-  d <- twListToDF(homeTimeline(n = x))
-  d <- d[1, 1] # 1=txt 11=usr
-  d <- as.character(d)
+  #d <- twListToDF(homeTimeline(n = x))
+  #d <- d[1, 1] # 1=txt 11=usr
+  #d <- as.character(d)
   #
   # Return a character string
   return(d)
 }
+
+get_tweet(x=1)
