3bf2a9f26d496815c49aa451a3a3ee2411059b73
maillot-jaune <stefan@inizio.se>
2014-04-18 11:40:55 +0200

    Testr

--- R/testr.R
+++ R/testr.R
@@ -2,25 +2,15 @@ testr <- function(x){
 #  y <- x + pi
 #  return(y)
   d <- tryCatch({
-      
-      local({
+
           load('/home/stefan/Desktop/twitter_auth.Rdata')
-        })
-#      d <- x + pi
-      
+
     }, warning = function(w) {
-      # warning-handler-code
-      #cat('Warning')
-      d <- paste('Warning',w)
+      d <- paste('Warning:', w)
     }, error = function(e) {
-      # error-handler-code
-      #cat('Error, somethin went wrong\n')
-      d <- paste('Error', e)
+      d <- paste('Error:', e)
     }, finally = {
-      # cleanup-code
-      #cat('Nothin to do, set d to arbitrary string \n')
-      d <- 'Clean up!'
+      d <- 'Unknown error...'
     })
-  
   return(d)
 }
