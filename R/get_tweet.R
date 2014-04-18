cbb3561e832d1c4cc0e2f70bb3bd764b39913898
maillot-jaune <stefan@inizio.se>
2014-04-18 10:25:47 +0200

    ocpu.seturl in index.html

--- R/get_tweet.R
+++ R/get_tweet.R
@@ -1,11 +1,20 @@
 get_tweet <- function(x){
-  library(twitteR)
+  # 
+  # Get the twitter auth file. This is just @si_petersson
+  # authentication. So the tweet returned by this function 
+  # will be the latest one in @si_petersson timeline.
   load('/home/stefan/Desktop/twitter_auth.Rdata')
-  
+  #
+  # Authentcate...
   registerTwitterOAuth(twitCred)
-  
+  #
+  # Clean up
   d <- twListToDF(homeTimeline(n = x))
   d <- d[1, 1] # 1=txt 11=usr
   d <- as.character(d)
+  #
+  # Return a character string
   return(d)
 }
+library(twitteR)
+get_tweet(x=1)
