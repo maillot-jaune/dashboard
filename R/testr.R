c66c27345ccfa9941f834dca4e1b21651a6aa848
maillot-jaune <stefan@inizio.se>
2014-04-18 11:26:51 +0200

    Testr

--- R/testr.R
+++ R/testr.R
@@ -11,13 +11,15 @@ testr <- function(x){
     }, warning = function(w) {
       # warning-handler-code
       #cat('Warning')
+      d <- 'Warning'
     }, error = function(e) {
       # error-handler-code
       #cat('Error, somethin went wrong\n')
+      d <- 'Error'
     }, finally = {
       # cleanup-code
       #cat('Nothin to do, set d to arbitrary string \n')
-      d <- 'Clean up and tweet me!'
+      d <- 'Clean up!'
     })
   
   return(d)
