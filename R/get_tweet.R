ebd83b1c57e1913a5c2a24afad3ac4c3ef71f528
maillot-jaune <stefan@inizio.se>
2014-04-20 13:06:46 +0200

    Testr

--- R/get_tweet.R
+++ R/get_tweet.R
@@ -4,16 +4,21 @@ get_tweet <- function(x){
   # authentication. So the tweet returned by this function 
   # will be the latest one in @si_petersson timeline.
 
- tryCatch({
-     tc <- file(description = '/home/stefan/Desktop/twitter_auth.Rdata', 
-       open = 'rb', 
-       blocking = TRUE, 
-       encoding = getOption('encoding')
-     )
-     
-     load(tc)
- 
-   }, warning = function(w) {
+  #local({
+  #  load('/home/stefan/Desktop/twitter_auth.Rdata')
+  #})
+  #load('/home/stefan/Desktop/twitter_auth.Rdata')
+  #
+  # Authentcate...
+  
+  tryCatch({
+      
+      registerTwitterOAuth(twitCred)
+      d <- twListToDF(homeTimeline(n = x))
+      d <- d[1, 1] # 1=txt 11=usr
+      d <- as.character(d)
+      
+    }, warning = function(w) {
       # warning-handler-code
       #cat('Warning')
     }, error = function(e) {
@@ -25,34 +30,6 @@ get_tweet <- function(x){
       d <- 'Clean up and tweet me!'
     })
  
- 
-  
-  #local({
-  #  load('/home/stefan/Desktop/twitter_auth.Rdata')
-  #})
-  #load('/home/stefan/Desktop/twitter_auth.Rdata')
-  #
-  # Authentcate...
-  
-#  tryCatch({
-#      
-#      registerTwitterOAuth(twitCred)
-#      d <- twListToDF(homeTimeline(n = x))
-#      d <- d[1, 1] # 1=txt 11=usr
-#      d <- as.character(d)
-#      
-#    }, warning = function(w) {
-#      # warning-handler-code
-#      #cat('Warning')
-#    }, error = function(e) {
-#      # error-handler-code
-#      #cat('Error, somethin went wrong\n')
-#    }, finally = {
-#      # cleanup-code
-#      #cat('Nothin to do, set d to arbitrary string \n')
-#      d <- 'Clean up and tweet me!'
-#    })
-# 
   #registerTwitterOAuth(twitCred)  
   #
   # Clean up
@@ -61,6 +38,5 @@ get_tweet <- function(x){
   #d <- as.character(d)
   #
   # Return a character string
-  d <- 1
   return(d)
 }
--- R/testr.R
+++ R/testr.R
@@ -1,16 +1,4 @@
 testr <- function(x){
-#  y <- x + pi
-#  return(y)
-  d <- tryCatch({
-
-      local({load('/home/stefan/Desktop/twitter_auth.Rdata')})
-
-    }, warning = function(w) {
-      d <- paste('Warning: ', w)
-    }, error = function(e) {
-      d <- paste('Error:', e)
-    }, finally = {
-      d <- 'Unknown error...'
-    })
-  return(d)
+  y <- x + pi
+  return(y)
 }
