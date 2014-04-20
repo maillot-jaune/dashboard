7ecb3320c69fa94ac84ef4f81b7e06fbf8ebf44f
maillot-jaune <stefan@inizio.se>
2014-04-20 13:55:08 +0200

    get_tweet

--- R/get_tweet.R
+++ R/get_tweet.R
@@ -3,39 +3,22 @@ get_tweet <- function(x){
   # Get the twitter auth file. This is just @si_petersson
   # authentication. So the tweet returned by this function 
   # will be the latest one in @si_petersson timeline.
-
-  #local({
-  #  load('/home/stefan/Desktop/twitter_auth.Rdata')
-  #})
-  #load('/home/stefan/Desktop/twitter_auth.Rdata')
+  twitter_authentication <- file(description = '/home/stefan/Desktop/twitter_auth.Rdata', 
+    open = 'rb', 
+    blocking = TRUE, 
+    encoding = getOption('encoding')
+  )
+  #
+  # Load the authentication data image into the current environment
+  local({load(twitter_authentication)}) 
   #
   # Authentcate...
-  
-  tryCatch({
-      
-      registerTwitterOAuth(twitCred)
-      d <- twListToDF(homeTimeline(n = x))
-      d <- d[1, 1] # 1=txt 11=usr
-      d <- as.character(d)
-      
-    }, warning = function(w) {
-      # warning-handler-code
-      #cat('Warning')
-    }, error = function(e) {
-      # error-handler-code
-      #cat('Error, somethin went wrong\n')
-    }, finally = {
-      # cleanup-code
-      #cat('Nothin to do, set d to arbitrary string \n')
-      d <- 'Clean up and tweet me!'
-    })
- 
-  #registerTwitterOAuth(twitCred)  
+  registerTwitterOAuth(twitCred)
   #
-  # Clean up
-  #d <- twListToDF(homeTimeline(n = x))
-  #d <- d[1, 1] # 1=txt 11=usr
-  #d <- as.character(d)
+  # Fix
+  d <- twListToDF(homeTimeline(n = x))
+  d <- d[1, 1] # 1=txt 11=usr
+  d <- as.character(d)
   #
   # Return a character string
   return(d)
