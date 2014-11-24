2ac991108be9cb84fafa45cd57e4944904dc963c
maillot-jaune <stefan@inizio.se>
2014-11-24 20:37:58 +0100

    Documentation...

--- R/get_tweet.R
+++ R/get_tweet.R
@@ -3,7 +3,7 @@ get_tweet <- function(x){
   # Get the twitter auth file. This is just @si_petersson
   # authentication. So the tweet returned by this function 
   # will be the latest one in @si_petersson timeline.
-  twitter_authentication <- file(description = '/home/stefan/Desktop/twitter_auth.Rdata', 
+  twitter_authentication <- file(description = '/etc/twitter_auth.Rdata', 
     open = 'rb', 
     blocking = TRUE, 
     encoding = getOption('encoding')
