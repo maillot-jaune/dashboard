445c3a3d4cc78204d001621a167a72ede8e56f06
maillot-jaune <stefan@inizio.se>
2014-11-28 17:07:43 +0100

    Deleted rplot function

--- R/testr.R
+++ R/testr.R
@@ -1,4 +1,4 @@
-testr <- function(ticker = "GOOG", from = "2013-01-01", to = Sys.time()){
-  mydata <- yahoodata(ticker, from, to);
-  qplot(Date, Close, data = mydata, geom = c("line", "smooth"));
+testr <- function(x){
+  res <- x*pi
+  return(res)
 }
