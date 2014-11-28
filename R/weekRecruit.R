21b42b367720b11d41f83ad52de59d8933e966c1
maillot-jaune <stefan@inizio.se>
2014-11-28 22:19:04 +0100

    SQL

--- R/weekRecruit.R
+++ R/weekRecruit.R
@@ -2,26 +2,25 @@ weekRecruit <- function(x){
 
 #  library(RMySQL)
 
-#  sql <- paste(scan('/home/dash/script/select_weekRecruit.SQL', 
+  sql <- paste(scan('/home/dash/script/select_weekRecruit.SQL', 
 #  sql <- paste(scan('/home/stefan/Desktop/select_weekRecruit.SQL', 
-#      what = 'character', 
-#      quiet = TRUE), 
-#    collapse = ' ')
-#  drv <- dbDriver('MySQL')
-#  con <- dbConnect(drv, 
-#    user = 'root', 
-#    host = '192.168.1.254', 
-#    dbname = 'dbSt', 
-#    password = 'four4u')
-#  dbGetQuery(con, 'SET NAMES "utf8"')
-#  res <- dbSendQuery(con, sql)
-#  d <- fetch(res, n = -1)
-#  mysqlCloseConnection(con)
+      what = 'character', 
+      quiet = TRUE), 
+    collapse = ' ')
+  drv <- dbDriver('MySQL')
+  con <- dbConnect(drv, 
+    user = 'root', 
+    host = '192.168.1.254', 
+    dbname = 'dbSt', 
+    password = 'four4u')
+  dbGetQuery(con, 'SET NAMES "utf8"')
+  res <- dbSendQuery(con, sql)
+  d <- fetch(res, n = -1)
+  mysqlCloseConnection(con)
   
 #  par(mar = c(2, 0, 2, 0))
   
-#  plot(d[ ,3],
-  plot(c(x,43,23,34,23,43,31),
+  plot(d[ ,3],
     type='l',
     ann = FALSE,
     axes = FALSE,
@@ -29,30 +28,30 @@ weekRecruit <- function(x){
     col= '#33cccc'
   )
   
-#  axis(1, 
-#    las = 1,
-#    col = '#cccccc',
-#    lwd = 5,
-#    col.axis = '#cccccc',
-#    at = axTicks(1),
-#    labels = substring(d[ ,2], 1, 2)
-#  )
-#  
-#  points(d[ ,3], 
-#    cex = 2,
-#    pch = 21,
-#    lwd = 5,
-#    col = '#33cccc',
-#    bg = '#ffffff',
-#    xpd = TRUE
-#  )
-#  
-#  text(x = 1:7, 
-#    y = d[ ,3], 
-#    labels = d[ ,3], 
-#    xpd= TRUE,
-#    col = '#cccccc',
-#    pos = 3
-#  )
+  axis(1, 
+    las = 1,
+    col = '#cccccc',
+    lwd = 5,
+    col.axis = '#cccccc',
+    at = axTicks(1),
+    labels = substring(d[ ,2], 1, 2)
+  )
+  
+  points(d[ ,3], 
+    cex = 2,
+    pch = 21,
+    lwd = 5,
+    col = '#33cccc',
+    bg = '#ffffff',
+    xpd = TRUE
+  )
+  
+  text(x = 1:7, 
+    y = d[ ,3], 
+    labels = d[ ,3], 
+    xpd= TRUE,
+    col = '#cccccc',
+    pos = 3
+  )
   
 }
