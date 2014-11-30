87f053b7486d865113e509aee05fe9fc9132226d
maillot-jaune <stefan@inizio.se>
2014-11-30 19:43:10 +0100

    New x-axis on day

--- R/dayRecruit.R
+++ R/dayRecruit.R
@@ -29,17 +29,22 @@ dayRecruit <- function(){
     lwd = 8,
     col= '#33cccc'
   )
-  
+    
   axis(1,
-    mgp = c(0, 1.5 , 0), # label, tick-mark label, tick-mark
+    mgp = c(0, 1.5 , 0), # Tick label distance from axis, label, tick-mark label, tick-mark
     las = 1,
     cex.axis = 2,
     font = 2,
     col = '#cccccc',
     lwd = 8,
     col.axis = '#cccccc',
-    at = seq(from = 0, to = 23, by = 3),
-    labels = formatC(seq(from = 0, to = 23, by = 3), width = 2, format = 'd', flag = '0')
+    at = seq(from = 1, to = 23, by = 3),
+    labels = formatC(seq(from = 0, 
+        to = 23, 
+        by = 3), 
+      width = 2, 
+      format = 'd', 
+      flag = '0')
   )
   
   points(d[ ,3], 
@@ -59,6 +64,5 @@ dayRecruit <- function(){
     xpd= TRUE,
     col = '#cccccc',
     pos = 3
-  )
-    
+  )    
 }
